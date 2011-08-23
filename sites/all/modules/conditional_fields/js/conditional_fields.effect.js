26bfaf8c08a8fcc9e6b802b8d6ad6a52615cf3bc
Mike Keran <info@mikekeran.com>
2011-08-23 15:14:41 -0700

    Upgraded to latest -dev to include date_popup support

--- sites/all/modules/conditional_fields/js/conditional_fields.effect.js
+++ sites/all/modules/conditional_fields/js/conditional_fields.effect.js
@@ -4,28 +4,58 @@
  * Override states.js "state:visible" event for fields with custom effects.
  */
 {
-  $(document).ready(function() {
-    $.each(Drupal.settings.conditionalFields.effect, function(dependent, dependee) {
-      $('[name="'+dependent+'"]').unbind('state:visible').bind('state:visible', function(e) {
-        if (e.trigger) {
-          var effect = Drupal.settings.conditionalFields.effect[$(e.target).attr('name')];
-          switch (effect) {
-            case 'fade':
-              $(e.target).closest('.form-item, .form-submit, .form-wrapper')[e.value ? 'fadeIn' : 'fadeOut']();
-              break;
-            case 'slide':
-              $(e.target).closest('.form-item, .form-submit, .form-wrapper')[e.value ? 'slideDown' : 'slideUp']();
-              break;
-            default:
-              // The "effect" variable is treated as a jQuery plugin.
-              $(e.target)[effect](e);
-          }
+
+  Drupal.behaviors.conditionalFields = {
+    attach: function (context, settings) {
+      $.each(settings.conditionalFields.effects, function(dependent) {
+        var effect = settings.conditionalFields.effects[dependent];
+        switch (effect.effect) {
+          case 'fade':
+            $('[name="'+dependent+'"]', context).unbind('state:visible').bind('state:visible', function(e) {
+              if (e.trigger) {
+                $(e.target).closest('.form-item, .form-submit, .form-wrapper')[e.value ? 'fadeIn' : 'fadeOut'](parseInt(effect.options.speed));
+              }
+              // Prevent bubbling of the event to document.
+              return false;
+            });
+            break;
+
+          case 'slide':
+            $('[name="'+dependent+'"]', context).unbind('state:visible').bind('state:visible', function(e) {
+              if (e.trigger) {
+                $(e.target).closest('.form-item, .form-submit, .form-wrapper')[e.value ? 'slideDown' : 'slideUp'](parseInt(effect.options.speed));
+              }
+              return false;
+            });
+            break;
+
+          case 'fill':
+          case 'empty':
+            $('[name="'+dependent+'"]', context).bind('state:empty', function(e) {
+              if (e.trigger) {
+                if (effect.options.reset) {
+                  if (typeof oldValue == 'undefined' || $(e.target).val() != effect.options.value) {
+                    oldValue = $(e.target).val();
+                  }
+                  $(e.target).val((effect.effect == 'fill' ? e.value : !e.value) ? oldValue : effect.options.value);
+                }
+                else {
+                  if (effect.effect == 'fill' && !e.value || effect.effect == 'empty' && e.value) {
+                    $(e.target).val(effect.options.value);
+                  }
+                }
+              }
+            });
+            break;
+
+          default:
+            // The "effect" variable is treated as a jQuery plugin.
+            $(e.target)[effect.effect](e, effect.options);
         }
-        // Prevent bubbling of the event to document.
-        return false;
       });
-    });
-  });
+    }
+  };
+
 }
 
 })(jQuery);
